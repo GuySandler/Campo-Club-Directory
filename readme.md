a6b66ad4b83442493bfbd4ab863853f403287fe0
Guy Sandler <guy@sandler.co.il>
2024-08-31 17:12:11 -0700

    cloud firebase connection

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 1. npm install
 2. npm install svelte-flowbite
+3. npm install generate-password-ts
+4. npm install firebase
--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,11 +1,17 @@
 <script>
     import { Textarea, Select, Label, Input, GradientButton, P, Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
     import campo from "../../lib/campo.svg"
+    import generator from 'generate-password-ts';
     let page = 0;
     function add() {page = 1;}
     function update() {page = 2;}
     function submitadd() {
-        alert("submitted");
+        // alert("submitted");
+        passgen();
+        submited = true;
+    }
+    function passgen() {
+        passcode = generator.generate({length: 10,numbers: true, symbols: true, uppercase: true, lowercase: true})
     }
     let types = [
         { value: 'academic', name: 'Academic Club' },
@@ -18,6 +24,13 @@
     let name = "";
     let when = "";
     let where = "";
+    let type = "";
+    let disc = "";
+    let email = "";
+    let submited = false;
+    let passcode = "";
+
+    let passcorrect = false;
 </script>
 <Navbar style="background-color:#44ddc8;" let:hidden let:toggle>
     <NavBrand href="/">
@@ -41,33 +54,74 @@
     {/if}
     {#if page == 1}
     <div style="width:75%;padding-top:30px;padding-bottom:30px;position:relative;top:5vh;border:1px black solid;border-radius:15px;margin-bottom:75px;">
-        <P size="xl" align="center" style="margin-bottom:10px;">Adding a new club</P>
-        <P align="center" style="margin-bottom:10px;">note that you will need to resubmit this every schoolyear</P>
-        <br>
-        <Label class="space-y-2 mb-5">
-            <span>Name of club</span>
-            <Input bind:value={name} style="width:30%;" placeholder="name" />
-        </Label>
-        <Label class="inline-block mr-5 space-y-2">
-            <span>Where do you meet?</span>
-            <Input bind:value={where} style="width:100%;" placeholder="room 10" />
-        </Label>
-        <Label class="inline-block ml-5 space-y-2">
-            <span>When do you meet?</span>
-            <Input bind:value={when} style="width:100%;" placeholder="Fidays at lunch" />
-        </Label>
-        <br>
-        <span>Club Type</span>
-        <Select id="types" style="width:50%;" underline class="mt-2" items={types} />
-        <br>
-        <Textarea style="width:75%;" placeholder="Discription of club" rows="4" name="message" />
-        <br>
-        <Label class="inline-block ml-5 space-y-2">
-            <span>Contact Email</span>
-            <Input type="email" pattern="[^ @]*@[^ @]*" style="width:100%;" placeholder="Email" />
-        </Label>
-        <br>
-        <GradientButton style="margin-top:10px;" on:click={submitadd} color="green">Submit</GradientButton>
+        {#if submited == false}
+            <P size="xl" align="center" style="margin-bottom:10px;">Adding a new club</P>
+            <P align="center" style="margin-bottom:10px;">note that you will need to resubmit this every schoolyear</P>
+            <br>
+            <Label class="space-y-2 mb-5">
+                <span>Name of club</span>
+                <Input bind:value={name} style="width:30%;" placeholder="name" />
+            </Label>
+            <Label class="inline-block mr-5 space-y-2">
+                <span>Where do you meet?</span>
+                <Input bind:value={where} style="width:100%;" placeholder="room 10" />
+            </Label>
+            <Label class="inline-block ml-5 space-y-2">
+                <span>When do you meet?</span>
+                <Input bind:value={when} style="width:100%;" placeholder="Fidays at lunch" />
+            </Label>
+            <br>
+            <span>Club Type</span>
+            <Select bind:value={type} id="types" style="width:50%;" underline class="mt-2" items={types} />
+            <br>
+            <Textarea bind:value={disc} style="width:75%;" placeholder="Discription of club" rows="4" name="message" />
+            <br>
+            <Label class="inline-block ml-5 space-y-2">
+                <span>Contact Email</span>
+                <Input bind:value={email} type="email" pattern="[^ @]*@[^ @]*" style="width:100%;" placeholder="Email" />
+            </Label>
+            <br>
+            <GradientButton style="margin-top:10px;" on:click={submitadd} color="green">Submit</GradientButton>
+        {/if}
+        {#if submited == true}
+            <P size="xl" align="center" style="margin-bottom:10px;">Thank you for submitting a club</P>
+            <P align="center" style="margin-bottom:10px;">Your club will be added to the list soon</P>
+            <P align="center" style="margin-bottom:10px;"><strong>Remember this passcode</strong> to edit your club later, if you forgot it <strong>TOO BAD</strong>: {passcode}</P>
+        {/if}
+    </div>
+    {/if}
+    {#if page == 2}
+    <div style="width:75%;padding-top:30px;padding-bottom:30px;position:relative;top:5vh;border:1px black solid;border-radius:15px;margin-bottom:75px;">
+        {#if passcorrect == false}
+            <P size="xl" align="center" style="margin-bottom:10px;">Update a Club</P>
+            <br>
+        {/if}
+        {#if passcorrect == true}
+            <Label class="space-y-2 mb-5">
+                <span>Name of club</span>
+                <Input bind:value={name} style="width:30%;" placeholder="name" />
+            </Label>
+            <Label class="inline-block mr-5 space-y-2">
+                <span>Where do you meet?</span>
+                <Input bind:value={where} style="width:100%;" placeholder="room 10" />
+            </Label>
+            <Label class="inline-block ml-5 space-y-2">
+                <span>When do you meet?</span>
+                <Input bind:value={when} style="width:100%;" placeholder="Fidays at lunch" />
+            </Label>
+            <br>
+            <span>Club Type</span>
+            <Select bind:value={type} id="types" style="width:50%;" underline class="mt-2" items={types} />
+            <br>
+            <Textarea bind:value={disc} style="width:75%;" placeholder="Discription of club" rows="4" name="message" />
+            <br>
+            <Label class="inline-block ml-5 space-y-2">
+                <span>Contact Email</span>
+                <Input bind:value={email} type="email" pattern="[^ @]*@[^ @]*" style="width:100%;" placeholder="Email" />
+            </Label>
+            <br>
+            <GradientButton style="margin-top:10px;" on:click={submitadd} color="green">Submit</GradientButton>
+        {/if}
     </div>
     {/if}
 </center>
